005e9a411e1ef17e6212091e4d66e95aa55515b1
zakkyfirdaus <zakky188@gmail.com>
2020-03-08 21:26:14 +0700

    Pembuatan file sql

---
+++ agenda.sql
@@ -0,0 +1,186 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 08 Mar 2020 pada 15.25
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `agenda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengajuan_rapat`
+--
+
+CREATE TABLE `pengajuan_rapat` (
+  `id` int(11) NOT NULL,
+  `tema` varchar(255) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jam` time NOT NULL,
+  `keterangan` int(11) NOT NULL,
+  `hasil_rapat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pengajuan_rapat`
+--
+
+INSERT INTO `pengajuan_rapat` (`id`, `tema`, `tanggal`, `jam`, `keterangan`, `hasil_rapat`) VALUES
+(6, 'penambahan karyawan baru', '2019-11-30', '12:00:00', 1, 'ok enjoy aman'),
+(7, 'tambah gaji', '2019-11-29', '01:00:00', 1, 'ok'),
+(8, 'Seminar internasional', '2019-12-12', '08:00:00', 1, 'ok'),
+(9, 'laporan bulanan', '2019-12-13', '03:02:00', 1, 'ok'),
+(10, 'open lowongan kerja', '2019-12-14', '02:02:00', 1, 'ok'),
+(11, 'pemecatan karyawan', '2019-12-15', '03:04:00', 1, 'ok'),
+(13, 'rapat bulanan', '2019-12-01', '14:00:00', 1, '1. akan ditambahkan karyawan baru'),
+(14, 'rapat mingguan', '2019-12-08', '12:00:00', 1, 'kkabdfkbsdkv ksjflsdg'),
+(15, 'rapat dengan vendor', '2020-03-26', '02:00:00', 0, ''),
+(16, 'rapat dengan otaku', '2020-03-31', '21:30:00', 1, 'Rapat dengan vendor pada ruangan III/D');
+
+--
+-- Trigger `pengajuan_rapat`
+--
+DELIMITER $$
+CREATE TRIGGER `info_rapat` AFTER INSERT ON `pengajuan_rapat` FOR EACH ROW BEGIN
+ INSERT INTO tugas_divisi SET
+ tema=NEW.tema, tanggal=new.tanggal, jam=new.jam;
+ END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tugas_divisi`
+--
+
+CREATE TABLE `tugas_divisi` (
+  `id` int(11) NOT NULL,
+  `tema` varchar(255) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jam` time NOT NULL,
+  `tujuan` varchar(50) NOT NULL,
+  `penanggung_jawab` varchar(255) NOT NULL,
+  `tugas` text NOT NULL,
+  `hasil_tugas` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tugas_divisi`
+--
+
+INSERT INTO `tugas_divisi` (`id`, `tema`, `tanggal`, `jam`, `tujuan`, `penanggung_jawab`, `tugas`, `hasil_tugas`) VALUES
+(2, 'pemecatan karyawan', '2019-11-16', '10:10:00', 'bussiness and development', '', 'laporan bisnis bulan 10', '26624Laporan bussiness and development (2).doc'),
+(3, 'op', '2019-11-20', '05:00:00', 'bussiness and development', '', 'a', '2662426624Laporan bussiness and development (2).doc'),
+(4, 'pemecatan karyawan', '2019-11-16', '10:10:00', 'finance, accounting, and tax', '', 'laporan bulanan 2019', '26624SURAT KUASA.doc'),
+(5, 'fhjsahfshfjh', '2019-10-10', '10:10:00', 'human resource and general', '', 'laporan bulanan 2011', ''),
+(6, 'pemecatan karyawan', '2019-11-16', '10:10:00', 'supply chain management', '', 'laporan keuangan 2020', ''),
+(7, 'penambahan karyawan baru', '2019-11-30', '12:00:00', '', '', '', ''),
+(8, 'penambahan karyawan baru', '2019-11-30', '12:00:00', 'bussiness and development', '', 'buat sistem yg baru', '2662426624SURAT KUASA.doc'),
+(9, 'penambahan karyawan baru', '2019-11-30', '12:00:00', 'finance, accounting, and tax', '', 'serah', '417455kawai.png'),
+(10, 'tambah gaji', '2019-11-29', '01:00:00', '', '', '', ''),
+(11, 'penambahan karyawan baru', '2019-11-30', '12:00:00', 'bussiness and development', '', 'KJH', '2662426624SURAT KUASA (1).doc'),
+(12, 'penambahan karyawan baru', '2019-11-30', '12:00:00', 'human resource and general', '', 'hsjdf', ''),
+(13, 'Seminar internasional', '2019-12-12', '08:00:00', '', '', '', ''),
+(14, 'laporan bulanan', '2019-12-13', '03:02:00', '', '', '', ''),
+(15, 'open lowongan kerja', '2019-12-14', '02:02:00', '', '', '', ''),
+(16, 'pemecatan karyawan', '2019-12-15', '03:04:00', '', '', '', '55035WebBanner_1.jpg'),
+(17, 'produk baru', '2019-12-20', '18:01:00', '', '', '', ''),
+(18, 'pemecatan karyawan', '2019-11-16', '10:10:00', 'bussiness and development', '', 'tugas 1', '2662426624Laporan bussiness and development (2).doc'),
+(19, 'rapat bulanan', '2019-12-01', '14:00:00', '', '', '', ''),
+(20, 'rapat bulanan', '2019-12-01', '14:00:00', 'finance, accounting, and tax', '', 'rekap keuangan bulan november', '279908kaneki.jpg'),
+(21, 'rapat mingguan', '2019-12-08', '12:00:00', '', '', '', ''),
+(22, 'rapat mingguan', '2019-12-08', '12:00:00', 'supply chain management', 'wawan kurniawan', 'kau  bersihkan ruangan kerja kau tu yaa\r\n', ''),
+(23, 'rapat mingguan', '2019-12-08', '12:00:00', 'supply chain management', 'awd', 'wqdqd', ''),
+(24, 'pemecatan karyawan', '2019-12-15', '03:04:00', 'bussiness and development', 'fasfda', 'adsdasd', '0'),
+(25, 'rapat dengan vendor', '2020-03-26', '02:00:00', '', '', '', ''),
+(26, 'rapat dengan otaku', '2020-03-31', '21:30:00', '', '', '', ''),
+(27, 'rapat dengan otaku', '2020-03-31', '21:30:00', 'bussiness and development', 'Pak Samidi', 'Dokumentasi', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `nip` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `tipe` varchar(255) NOT NULL,
+  `foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`nip`, `nama`, `password`, `tipe`, `foto`) VALUES
+(1, 'gjhack', '1', 'pimpinan', 'gjhack.jpg'),
+(2, 'Zakky Firdaus', '1', 'admin', 'ridho darmawan.jpg'),
+(3, 'info', '1', 'pegawai', 'info.jpg'),
+(5, 'Administrator Sekretariat Utama', '1', 'sekretariat utama', 'rona.jpg'),
+(6, 'Administrator Deputi 1', '1', 'deputi 1', 'rini.jpg'),
+(8, 'Administrator Deputi 2', '1', 'deputi 2', 'fitra.jpg'),
+(9, 'Administrator Deputi 3', '1', 'deputi 3', 'Administrator Deputi 4.jpg'),
+(10, 'Administrator Deputi 4', '1', 'deputi 4', 'Administrator Deputi 4.jpg'),
+(11, 'Administrator Deputi 5', '1', 'deputi 5', 'Administrator Deputi 5.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `pengajuan_rapat`
+--
+ALTER TABLE `pengajuan_rapat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tugas_divisi`
+--
+ALTER TABLE `tugas_divisi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`nip`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `pengajuan_rapat`
+--
+ALTER TABLE `pengajuan_rapat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT untuk tabel `tugas_divisi`
+--
+ALTER TABLE `tugas_divisi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
